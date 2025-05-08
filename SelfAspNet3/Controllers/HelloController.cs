540ace8ee9f8c2f030a16e3dc15315b7b80384cd
Toshi Yoshida <hi-toshi@outlook.com>
2025-05-08 21:01:15 +0900

    Add project files.

---
+++ SelfAspNet3/Controllers/HelloController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders.Composite;
+
+namespace SelfAspNet3.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("こんにちは　世界!");
+        }
+    }
+}
